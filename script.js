cc586b822b9c8b2c119b6727038a5166b34bae0d
Christian Flores <104774677+AmbitiousFlo@users.noreply.github.com>
2023-07-05 18:06:50 -0500

    Made changes to css and javascript
    Added the class btn to css file.
    Added comments to javascript file and continued to update the file with an Event listener, Next button changes. Once you select an answer the pointer is disabled.

---
+++ script.js
@@ -0,0 +1,140 @@
+// Array of objects containing the question and answer options
+
+const questions = [
+  {
+    question: "Which is the largest animal in the world?",
+    answers: [
+      { text: "Shark", correct: false },
+      { text: "Blue whale", correct: true },
+      { text: "Elephant", correct: false },
+      { text: "Giraffe", correct: false },
+    ]
+  },
+  {
+    question: "Which is the largest animal in the world?",
+    answers: [
+      { text: "Shark", correct: false },
+      { text: "Blue whale", correct: true },
+      { text: "Elephant", correct: false },
+      { text: "Giraffe", correct: false },
+    ]
+  },
+  {
+    question: "Which is the largest animal in the world?",
+    answers: [
+      { text: "Shark", correct: false },
+      { text: "Blue whale", correct: true },
+      { text: "Elephant", correct: false },
+      { text: "Giraffe", correct: false },
+    ]
+  },
+  {
+    question: "Which is the largest animal in the world?",
+    answers: [
+      { text: "Shark", correct: false },
+      { text: "Blue whale", correct: true },
+      { text: "Elephant", correct: false },
+      { text: "Giraffe", correct: false },
+    ]
+  },
+  {
+    question: "Which is the largest animal in the world?",
+    answers: [
+      { text: "Shark", correct: false },
+      { text: "Blue whale", correct: true },
+      { text: "Elephant", correct: false },
+      { text: "Giraffe", correct: false },
+    ]
+  }
+];
+
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+// Initialize the quiz by setting the current question index and score
+// Display the "Next" button and show the first question
+function startQuiz() {
+  currentQuestionIndex = 0;
+  score = 0;
+  nextButton.innerHTML = "Next";
+  showQuestion();
+}
+
+// Display the current question and its answer options
+// Add event listeners to the answer buttons
+function showQuestion() {
+  resetState();
+  let currentQuestion = questions[currentQuestionIndex];
+  let questionNo = currentQuestionIndex + 1;
+  questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+  currentQuestion.answers.forEach(answer => {
+    const button = document.createElement("button");
+    button.innerHTML = answer.text;
+    button.classList.add("btn");
+    answerButtons.appendChild(button);
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener("click", selectAnswer);
+  });
+}
+
+// Reset the state of the quiz by hiding the "Next" button and clearing the answer buttons
+function resetState() {
+  nextButton.style.display = "none";
+  while (answerButtons.firstChild) {
+    answerButtons.removeChild(answerButtons.firstChild);
+  }
+}
+
+// Handle the selection of an answer by the user
+// Update the score, apply feedback to the selected button
+// Disable all buttons, and display the "Next" button
+function selectAnswer(e) {
+  const selectedBtn = e.target;
+  const isCorrect = selectedBtn.dataset.correct === "true";
+  if (isCorrect) {
+    selectedBtn.classList.add("correct");
+    score++;
+  } else {
+    selectedBtn.classList.add("incorrect");
+  }
+  Array.from(answerButtons.children).forEach(button => {
+    if (button.dataset.correct == "true") {
+      button.classList.add("correct");
+    }
+    button.disabled = true;
+  });
+  nextButton.style.display = "block";
+}
+
+function showScore() {
+  resetState();
+  questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+  nextButton.innerHTML = "Play Again";
+  nextButton.style.display = "block";
+}
+
+function handleNextButton() {
+  currentQuestionIndex++;
+  if (currentQuestionIndex < questions.length) {
+    showQuestion();
+  } else {
+    showScore();
+  }
+}
+
+nextButton.addEventListener("click", () => {
+  if (currentQuestionIndex < questions.length) {
+    handleNextButton();
+  } else {
+    startQuiz();
+  }
+});
+
+startQuiz();

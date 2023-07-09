75124aa9419887f737c46d54d1f8bc97a4e42c1a
Christian Flores <104774677+AmbitiousFlo@users.noreply.github.com>
2023-07-09 16:24:36 -0500

    Made changes to JS and HTML

---
+++ highscores.js
@@ -0,0 +1,24 @@
+const highScore = document.querySelector("#highscore")
+const clear = document.querySelector("#clear")
+const goBack = document.querySelector("#goBack")
+
+// Event Listener to clear scores
+clear.addEventListener("click", function () {
+    localStorage.clear();
+    location.reload();
+});
+
+// Retrieve storage locally
+const allScores = localStorage.getItem("allScores");
+allScores = JSON.parse(allScores);
+
+if (allScores !== null) {
+    for (const i = 0; i < allScores.length; i++) {
+        const createLi = document.createElement("li");
+        highScore.appendChild(createLi);
+    }
+}
+// Event listener to move to main page
+goBack.addEventListener("click", function () {
+    window.location.replace("./main.html");
+});
